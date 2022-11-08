a5e240d63e6195f12ad344960b48972860d13d64
Thedestructive <perguzerick@gmail.com>
2022-11-07 19:55:43 -0600

    Corrección de errores menores
    Se modifico un poco la interfaz.

--- app/src/main/java/com/gtappdevelopers/noteapplication/NoteDatabase.kt
+++ app/src/main/java/com/gtappdevelopers/noteapplication/NoteDatabase.kt
@@ -11,14 +11,12 @@ abstract class NoteDatabase : RoomDatabase() {
     abstract fun getNotesDao(): NotesDao
 
     companion object {
-        // Singleton prevents multiple instances of database opening at the
-        // same time.
+
         @Volatile
         private var INSTANCE: NoteDatabase? = null
 
         fun getDatabase(context: Context): NoteDatabase {
-            // if the INSTANCE is not null, then return it,
-            // if it is, then create the database
+
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
@@ -26,7 +24,6 @@ abstract class NoteDatabase : RoomDatabase() {
                     "note_database"
                 ).build()
                 INSTANCE = instance
-                // return instance
                 instance
             }
         }
